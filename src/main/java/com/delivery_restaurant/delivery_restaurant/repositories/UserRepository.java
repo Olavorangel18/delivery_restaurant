df7f72dff17a7fabc4e999e32ddd236156577d00
Olavo Conceicao <orc@cin.ufpe.br>
2022-08-07 17:59:50 -0300

    refatoração e criação de arquivo de segurança

--- src/main/java/com/delivery_restaurant/delivery_restaurant/repositories/UserRepository.java
+++ src/main/java/com/delivery_restaurant/delivery_restaurant/repositories/UserRepository.java
@@ -1,7 +1,8 @@
-package com.delivery_restaurant.repositories;
+package com.delivery_restaurant.delivery_restaurant.repositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import com.delivery_restaurant.models.User;
+
+import com.delivery_restaurant.delivery_restaurant.models.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User,String> {
---
+++ src/main/java/com/delivery_restaurant/delivery_restaurant/security/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.delivery_restaurant.delivery_restaurant.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import java.lang.Exception;
+
+ 
+@Configuration
+public class SecurityConfiguration {
+ 
+ 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        return http.build();
+        
+
+    }
+
+ 
+}
+    
+
