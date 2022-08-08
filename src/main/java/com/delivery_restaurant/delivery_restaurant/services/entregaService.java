0e5af6063750201f7eb7bae66a4fd332ccf464bb
Olavo Conceicao <orc@cin.ufpe.br>
2022-08-08 00:33:01 -0300

    repository e service de entrega criados

---
+++ src/main/java/com/delivery_restaurant/delivery_restaurant/services/entregaService.java
@@ -0,0 +1,45 @@
+package com.delivery_restaurant.delivery_restaurant.services;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.delivery_restaurant.delivery_restaurant.models.Entrega;
+import com.delivery_restaurant.delivery_restaurant.repositories.EntregaRepository;
+@Service
+
+public class entregaService implements generalService<Entrega> {
+        
+        @Autowired
+        private EntregaRepository entregaRepository;
+        
+        @Override
+        public List<Entrega> getAll() {
+            return this.entregaRepository.findAll();
+        }
+        
+        @Override
+        public Entrega getById(String id) {
+            return this.entregaRepository.findById(id).get();
+        }
+        
+        @Override
+        public Entrega save(Entrega entrega) {
+            return this.entregaRepository.save(entrega);
+        }
+        
+        @Override
+        public void delete(Entrega entrega) {
+            this.entregaRepository.delete(entrega);
+        }
+        
+        @Override
+        public void deleteById(String id) {
+            this.entregaRepository.deleteById(id);
+        }
+        
+        @Override
+        public void update(Entrega entrega) {
+            this.entregaRepository.save(entrega);
+        }
+        
+    
+}
